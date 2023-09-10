bbe952ab843549b3d2404d2b62cbf12a0947d15d
nndty <nndty136@outlook.com>
2023-09-10 12:44:30 +0800

    增加了蓝牙调试模块，同时开启一个频闪灯

--- ESP32Demo/src/main.cpp
+++ ESP32Demo/src/main.cpp
@@ -1,18 +1,37 @@
+
 #include <Arduino.h>
+#include <BluetoothSerial.h>
 
-// put function declarations here:
-int myFunction(int, int);
+BluetoothSerial SerialBT;
+void setup()
+{
+    // 蓝牙模块
+    Serial.begin(115200);
+    SerialBT.begin("nndty"); // 如果没有参数传入则默认是蓝牙名称是: "ESP32"
+    // SerialBT.setPin("123456");   // 蓝牙连接的配对码
+    Serial.printf("BT initial ok and ready to pair. \r\n");
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    // 将小灯引脚设为输出模式
+    pinMode(2, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop()
+{
+    // 蓝牙模块
+    if (Serial.available())
+    {
+        SerialBT.write(Serial.read());
+    }
+    if (SerialBT.available())
+    {
+        Serial.write(SerialBT.read());
+    }
+    delay(1);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+
+    // 频闪小灯
+    analogWrite(2, 1000); // 点亮LED
+    delay(500); // 延时500毫秒
+    analogWrite(2, 0); // 关闭LED
+    delay(500); // 延时500毫秒
 }
